52494aa8e205c21f411e9abe86de9189d20a1f1b
Денис Коропов <denkoropov@gmail.com>
2022-09-06 15:01:18 +0300

    Какой то комит

--- bitrix/managed_cache/MYSQL/b_option/7f/7f336d6eda3808bc83a7c6ae49bbbb69.php
+++
@@ -1,7 +0,0 @@
-<?
-if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001661823425';
-$dateexpire = '001661827025';
-$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:1:{s:1:"-";a:0:{}}}';
-return true;
-?>
--- bitrix/modules/main/admin/define.php
+++
@@ -1,1 +0,0 @@
-<?define("TEMPORARY_CACHE", "ARtqfAYHb2MMdwgebRtnG2YA");?>
